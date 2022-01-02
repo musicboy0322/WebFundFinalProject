bc76ce1fd8f60128125a387c4314556cc64bf4f5
Kyle <plusultra0322@gmail.com>
2022-01-03 02:36:26 +0800

    fixing layout

---
+++ drawStraws.js
@@ -0,0 +1,4 @@
+let numberNorth = ["ponkan" , "peach", "persimmon", "strawberry"]
+let numberWest = ["grape", "loquat", "orange", "pear", "mei", ] 
+let numberEast = ["plum", "sakyamuni", "watermelon"]
+let numberSouth = ["guava", "mango", "pomelo", "waxapple"]
